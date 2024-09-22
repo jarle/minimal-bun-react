79c329e4793ad08d97a6d9e5014bb4023f9c9aab
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-09-23 01:21:42 +0200

    commit

--- src/my-component.tsx
+++ src/my-component.tsx
@@ -1,7 +1,8 @@
 export function Component(props: { path: string }) {
   return (
-    <body>
-      <h1 className="hover:underline">{`Hello from ${props.path}`}</h1>
-    </body>
+    <div className="flex flex-col items-center p-12 gap-7">
+      <h1 className="hover:underline text-3xl font-bold">{`Hello from ${props.path}`}</h1>
+      <p>This is a server rendered component</p>
+    </div>
   );
 }
